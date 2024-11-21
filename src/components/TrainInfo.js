98bbf13852f708fd7ae178a69b406b3f80eec952
Matteo Delton <deltonmatteo@gmail.com>
2024-11-21 16:01:05 +0100

    Translate to Italian and display results better

--- src/components/TrainInfo.js
+++ src/components/TrainInfo.js
@@ -29,19 +29,54 @@ function TrainInfo({
 		return (
 			<div className="p-4 bg-gray-100 border rounded">
 				<button type="button" onClick={onClose} className="mb-4 text-red-500">
-					Close
+					Chiudi
 				</button>
-				<div>No data available</div>
+				<div>Caricamento…</div>
 			</div>
 		);
 	}
+
+	const formatTrack = (station, stops) => {
+		const track =
+			station.actual_departure_track || station.actual_arrival_track;
+		const scheduledTrack =
+			station.scheduled_departure_track || station.scheduled_arrival_track;
+
+		let formattedTrack = track;
+
+		if (track) {
+			if (track === scheduledTrack) {
+				formattedTrack = <span style={{ color: "blue" }}>{track}</span>;
+			} else if (scheduledTrack) {
+				formattedTrack = <span style={{ color: "magenta" }}>{track}</span>;
+			} else {
+				formattedTrack = <span style={{ color: "cyan" }}>{track}</span>;
+			}
+		} else {
+			formattedTrack = "N/A";
+		}
+
+		const origin = stops.find((stop) => stop.type === "departure");
+		const arrival = stops.find((stop) => stop.type === "arrival");
+
+		if (
+			(station.arrived || origin.actual_departure_time) &&
+			(station.departed || arrival.actual_arrival_time)
+		) {
+			formattedTrack = <span className="text-gray-500">{track}</span>;
+		} else if (station.arrived && !station.departed) {
+			formattedTrack = <span className="animate-pulse">{track}</span>;
+		}
+
+		return formattedTrack;
+	};
 
 	return (
 		<div className="p-4 bg-gray-100 border rounded">
 			<button type="button" onClick={onClose} className="mb-4 text-red-500">
 				Close
 			</button>
-			<h3 className="text-xl font-bold mb-2">Train {trainNumber}</h3>
+			<h3 className="text-xl font-bold mb-2">Treno {trainNumber}</h3>
 			<p>
 				{trainInfo.origin} · {formatIsoTime(trainInfo.departure_time)}
 			</p>
@@ -53,13 +88,13 @@ function TrainInfo({
 				<table className="min-w-full bg-white border">
 					<thead>
 						<tr>
-							<th className="py-2 px-4 border">Station</th>
-							<th className="py-2 px-4 border">Scheduled Departure</th>
-							<th className="py-2 px-4 border">Actual Departure</th>
-							<th className="py-2 px-4 border">Scheduled Arrival</th>
-							<th className="py-2 px-4 border">Actual Arrival</th>
-							<th className="py-2 px-4 border">Scheduled Track</th>
-							<th className="py-2 px-4 border">Actual Track</th>
+							<th className="py-2 px-4 border">Stazione</th>
+							<th className="py-2 px-4 border">Arrivo programmato</th>
+							<th className="py-2 px-4 border">Arrivo effettivo</th>
+							<th className="py-2 px-4 border">Partenza programmata</th>
+							<th className="py-2 px-4 border">Partenza effettiva</th>
+							<th className="py-2 px-4 border">Binario programmato</th>
+							<th className="py-2 px-4 border">Binario effettivo</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -82,58 +117,54 @@ function TrainInfo({
 										{stop.name}
 									</span>
 								</td>
-								{index !== 0 && (
+								{stop.type === "cancelled" ? (
+									<td className="py-2 px-4 border" colSpan="6">
+										Cancellato
+									</td>
+								) : (
 									<>
+										{index !== 0 && (
+											<>
+												<td className="py-2 px-4 border">
+													{formatIsoTime(stop.scheduled_arrival_time)}
+												</td>
+												<td className="py-2 px-4 border">
+													{stop.actual_arrival_time
+														? getFormattedTime(stop, { checkDepartures: false })
+														: "N/A"}
+												</td>
+											</>
+										)}
+										{index === 0 && (
+											<td className="py-2 px-4 border" colSpan="2">
+												Stazione di partenza
+											</td>
+										)}
+										{index !== trainInfo.stops.length - 1 && (
+											<>
+												<td className="py-2 px-4 border">
+													{formatIsoTime(stop.scheduled_departure_time)}
+												</td>
+												<td className="py-2 px-4 border">
+													{stop.actual_departure_time
+														? getFormattedTime(stop, { checkDepartures: true })
+														: "N/A"}
+												</td>
+											</>
+										)}
+										{index === trainInfo.stops.length - 1 && (
+											<td className="py-2 px-4 border" colSpan="2">
+												Stazione di arrivo
+											</td>
+										)}
 										<td className="py-2 px-4 border">
-											{stop.type === "cancelled"
-												? "Cancelled"
-												: formatIsoTime(stop.scheduled_departure_time)}
+											{stop.scheduled_departure_track || "N/A"}
 										</td>
 										<td className="py-2 px-4 border">
-											{stop.type === "cancelled"
-												? "Cancelled"
-												: stop.actual_departure_time
-													? getFormattedTime(stop, { checkDepartures: true })
-													: "N/A"}
+											{formatTrack(stop, trainInfo.stops)}
 										</td>
 									</>
 								)}
-								{index === 0 && (
-									<td className="py-2 px-4 border" colSpan="2">
-										Origin Station
-									</td>
-								)}
-								{index !== trainInfo.stops.length - 1 && (
-									<>
-										<td className="py-2 px-4 border">
-											{stop.type === "cancelled"
-												? "Cancelled"
-												: formatIsoTime(stop.scheduled_arrival_time)}
-										</td>
-										<td className="py-2 px-4 border">
-											{stop.type === "cancelled"
-												? "Cancelled"
-												: stop.actual_arrival_time
-													? getFormattedTime(stop, { checkDepartures: false })
-													: "N/A"}
-										</td>
-									</>
-								)}
-								{index === trainInfo.stops.length - 1 && (
-									<td className="py-2 px-4 border" colSpan="2">
-										Destination Station
-									</td>
-								)}
-								<td className="py-2 px-4 border">
-									{stop.type === "cancelled"
-										? "Cancelled"
-										: stop.scheduled_departure_track || "N/A"}
-								</td>
-								<td className="py-2 px-4 border">
-									{stop.type === "cancelled"
-										? "Cancelled"
-										: stop.actual_departure_track || "N/A"}
-								</td>
 							</tr>
 						))}
 					</tbody>
